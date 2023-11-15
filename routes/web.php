a2f0d5bb73faf06d6a16a96c6f592b708befbf65
Lamia Karam <20198120@stud.fci-cu.edu.eg>
2023-11-15 17:27:26 +0200

    v1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,79 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('test', function(){
+    return 'welcome to my first route';
+});
+
+Route::get('user/{name}', function($name){
+    return 'The user name is'. $name;
+});
+
+// Route::get('user/{name}/{age}', function($name, $age){
+//     $msg = 'The user name is'. $name;
+// });
+
+// Route::get('user/{name}/{age?}', function($name, $age=0){
+//     $msg = 'The user name is'. $name;
+//     if($age > 0){
+//         $msg.= 'and age is:'. $age;
+//     }
+//     return $msg;    
+// })->where(['age' => '[0,9]+']);
+
+Route::get('user/{name}/{age?}', function($name, $age=0){
+    $msg = 'The user name is'. $name;
+    if($age > 0){
+        $msg.= 'and age is:'. $age;
+    }
+    return $msg;    
+})->whereIn('name', ['Lamia', 'lamia']);
+
+Route::get('About', function(){
+    return 'Tis is website about page';
+});
+
+Route::get('contact_us', function(){
+    return 'you can contact us from our wbsite';
+});
+
+Route::prefix('support')->group(function(){
+    Route::get('/', function(){
+        return 'support home page';
+    });
+
+    Route::get('Chat', function(){
+        return 'chat page';
+    });
+
+    Route::get('call', function(){
+        return 'call page';
+    });
+
+    Route::get('ticket', function(){
+        return 'ticket page';
+    });
+});
+
+Route::prefix('training')->group(function(){
+    Route::get('/', function(){
+        return 'training home page';
+    });
+
+    Route::get('Hr', function(){
+        return 'Hr page';
+    });
+
+    Route::get('ICT', function(){
+        return 'ICT page';
+    });
+
+    Route::get('marketing', function(){
+        return 'marketing page';
+    });
+
+    Route::get('logistics', function(){
+        return 'logistics page';
+    });
+});
